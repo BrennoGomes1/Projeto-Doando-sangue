59440bb96f16f75a72ed4fcddcccfe2162f0d937
Brenno Gomes <brennosantos877@gmail.com>
2026-03-12 14:33:45 -0300

    Primeira modificação

--- projeto_ficha_cadastral.py
+++ projeto_ficha_cadastral.py
@@ -4,9 +4,15 @@ peso = float(input("Por favor, informe o seu peso em kg: "))
 altura = int(input("Por favor, informe sua altura em cm: "))
 ano_nascimento = int(input("Por favor, me informa seu ano de nascimento: "))
 
-idade = 2026 - ano_nascimento
+import datetime
+ano_atual = datetime.datetime.now().year
+idade = ano_atual - ano_nascimento
 peso_minimo = peso > 50
 idade_minima = idade >= 16
+if peso_minimo and idade_minima:
+    print("Pode doar sangue")
+else:
+    print("Não pode doar sangue")
 
 texto_saida = f"\tNOME: {nome}\n\tPESO: {peso} kg\n\tALTURA: {altura} cm \n\tIDADE: {idade}\n\tTEM PESO MINIMO PARA DOAR: {peso_minimo}\n\tTEM IDADE MINIMA PARA DOAR: {idade_minima}"
 
